38c44512b368ef86f3125ae36561c113d627222e
Richard <kasonde.nsama@gmail.com>
2022-08-19 16:35:41 +0200

    Updated workfow app-name and server URL config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,7 @@
-    // path: ./config/env/production/server.js
+// path: ./config/env/production/server.js
 
-    module.exports = ({ env }) => ({
-      host: env("HOST", "0.0.0.0"),
-      port: env.int("PORT", 1337),
-      url: "https://my-strapi-app-app.azurewebsites.net",
-      admin: {
-        url: "https://brave-bay-04250540f.1.azurestaticapps.net",
-        serveAdminPanel: false,
-      },
-    });
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://richards-strapi-app.azurewebsites.net",
+});
